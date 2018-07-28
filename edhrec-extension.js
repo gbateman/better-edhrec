8be9a459a9b8857be2216126ec1bbc477f9ac4a3
Greg Bateman <gerbot150@gmail.com>
2018-07-28 13:58:10 -0400

    Added scryfall links

--- edhrec-extension.js
+++ edhrec-extension.js
@@ -1,2 +1,15 @@
-var containers = document.getElementsByClassName("test_cardlistscontainer");
-containers[0].className = "cardlistscontainer";
+const containers = document.getElementsByClassName('test_cardlistscontainer');
+containers[0].className = 'cardlistscontainer';
+
+const cards = document.getElementsByClassName('oneimage');
+
+for(let i = 0; i < cards.length; i++) {
+  const link = (cards[i].getAttribute('data-src') || cards[i].src)
+    .replace('img.', '')
+    .replace(/s\/normal\/\w*(?=\/)/g, '')
+    .replace(/\.jpg\?\d*/g, '');
+  const anchor = document.createElement('a');
+  anchor.setAttribute('href', link);
+  anchor.setAttribute('style', 'position: absolute; top: 0; right: 0; bottom: 50%; left: 0; z-index: 1;');
+  cards[i].parentElement.appendChild(anchor);
+}
